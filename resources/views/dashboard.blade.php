d448e9faab060357ec3383aca3f8669c891348c4
nano.s <ti@optimajasa@gmail.com>
2022-12-30 10:24:48 +0700

    update dashboar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,59 +7,85 @@
     'subtitle' => 'Dahsboard Overview'
 ])
     <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
+<section class="content">
+    <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">
-                    Menu Data
+                    Daftar Pekerjaan
                 </h3>
             </div>
+            
             <div class="card-body">
                 <div class="row">
                     @include('widgets.dashboard_box', [
-                        "name" => "Draf",
-                        "total" => "<h4>0</h4>",
+                        "name" => "Surat yang perlu diproses",
+                        "total" => "<a href='/admin/proses-surat/' style='color:white'><h4>0</h4></a>",
                         "color" => "info",
                         "size" => "col"
                     ])
                     @include('widgets.dashboard_box', [
-                        "name" => "Permintaan Disetujui",
-                        "total" => "<h4>0</h4>",
-                        "color" => "success",
-                        "size" => "col"
-                    ])
-                    @include('widgets.dashboard_box', [
-                        "name" => "Tidak Disetujui",
-                        "total" => "<h4>0</h4>",
-                        "color" => "warning",
-                        "size" => "col"
-                    ])
-                   
-                    @include('widgets.dashboard_box', [
                         "name" => "Surat Masuk",
                         "total" => "<h4>0</h4>",
-                        "color" => "danger",
+                        "color" => "warning",
                         "size" => "col"
                     ])
 
                      @include('widgets.dashboard_box', [
                         "name" => "Disposisi",
                         "total" => "<h4>0</h4>",
-                        "color" => "succes",
+                        "color" => "success",
+                        "size" => "col"
+                    ])
+                </div>
+        </div>
+    </div>
+</section>
+<section class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">
+                    List Surat
+                </h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    @include('widgets.dashboard_box', [
+                        "name" => "Draft",
+                        "total" => "<h4></h4>",
+                        "color" => "info",
+                        "size" => "col"
+                    ])
+                    @include('widgets.dashboard_box', [
+                        "name" => "On Approval",
+                        "total" => "<h4></h4>",
+                        "color" => "yellow",
                         "size" => "col"
                     ])
 
-                       @include('widgets.dashboard_box', [
-                        "name" => "Total Permintaan",
-                        "total" => "<h4>0</h4>",
-                        "color" => "primary",
+                     @include('widgets.dashboard_box', [
+                        "name" => "Approved",
+                        "total" => "<h4></h4>",
+                        "color" => "success",
                         "size" => "col"
                     ])
-                <!-- ./col -->
+                    @include('widgets.dashboard_box', [
+                        "name" => "Rejected",
+                        "total" => "<h4></h4>",
+                        "color" => "danger",
+                        "size" => "col"
+                    ])
+                    @include('widgets.dashboard_box', [
+                        "name" => "Total Surat",
+                        "total" => "<h4></h4>",
+                        "color" => "grey",
+                        "size" => "col"
+                    ])
                 </div>
+            </div>
         </div>
-      </div>
-    </section>
+    </div>
+</section>
 @endsection
